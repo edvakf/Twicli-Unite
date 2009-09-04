db7305822f588e0df07b3274d4d57ac165256594
edvakf <taka.atsushi@gmail.com>
2009-09-04 01:19:45 -0400

    now resolves cli.gs & twurl.nl

--- public_html/unite_longer_url.js
+++ public_html/unite_longer_url.js
@@ -1,6 +1,6 @@
 /* replace short URLs with original URLs */
 (function(){
-  var re = /^http:\/\/(?:tinyurl\.com|bit\.ly|is\.gd|u\.nu|icio\.us|tr\.im)\//;
+  var re = /^http:\/\/(?:tinyurl\.com|bit\.ly|is\.gd|u\.nu|icio\.us|tr\.im|cli\.gs|twurl\.nl)\//;
 
   function resolveUrl(url){
     requestUrl = location.href.replace(/[^\/]*$/,'resolveurl') + '?url=' + encodeURIComponent(url);
