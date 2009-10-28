7097138e9053916a52272f65b4294da8a871ed0e
Atsushi Takayama <atsushi@utopia.local>
2009-10-28 00:14:07 -0400

    update to follow the original

--- src/public_html/followers.js
+++ src/public_html/followers.js
@@ -1,43 +1,30 @@
-var followers_ids_list = (readCookie('followers_ids') || '');
-followers_ids_list = followers_ids_list != '' ? followers_ids_list.split(',') : [];
-var followers_ids = [];
-for (var i = 0; i < followers_ids_list.length; i++)
-	followers_ids[followers_ids_list[i]] = 1;
-
-registerPlugin({
-	miscTab: function(ele) {
-		var e = document.createElement("p");
-		e.innerHTML = '<form onSubmit="twfcFollwersIDsRenew(); return false;">Color followers: <span id="followers_status">'+(followers_ids_list.length?"on("+followers_ids_list.length+")":"off")+'</span> <input type="submit" value="Renew"><input type="button" onClick="twfcFollwersIDsClear()" value="Off"> <a href="javascript:alert(\'Status Coloring:\\n  follower: black  non-follower: blue\')">[?]</a></form>';
-		ele.appendChild(e);
-		ele.appendChild(document.createElement("hr"));
-	},
-	newMessageElement: function(ele, tw) {
-		if (followers_ids_list.length && myid != tw.user.id && !followers_ids[tw.user.id])
-			for (var i = 0; i  < ele.childNodes.length; i++)
-				if (ele.childNodes[i].className == "status")
-					ele.childNodes[i].style.color = "#008";
-	}
-});
-
-function twfcFollwersIDsClear() {
-	followers_ids_list = followers_ids = [];
-	var status = document.getElementById("followers_status");
-	if (status) status.innerHTML = "off";
-	writeCookie('followers_ids', "", 3652);
-}
-function twfcFollwersIDsRenew() {
-	$("loading").style.display = "block";
-	var status = document.getElementById("followers_status");
-	if (status) status.innerHTML = "loading...";
-	xds.load(twitterURL + 'followers/ids.json', twfcRenew);
-}
-function twfcRenew(list) {
-	followers_ids_list = list;
-	followers_ids = [];
-	for (var i = 0; i < list.length; i++)
-		followers_ids[list[i]] = 1;
-	writeCookie('followers_ids', list.join(","), 3652);
-	var status = document.getElementById("followers_status");
-	if (status) status.innerHTML = "on (" + list.length + ")";
-	$("loading").style.display = "none";
-}
+Traceback (most recent call last):
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/web/api.py", line 339, in send_error
+    'text/html')
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/web/chrome.py", line 684, in render_template
+    data = self.populate_data(req, data)
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/web/chrome.py", line 592, in populate_data
+    d['chrome'].update(req.chrome)
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/web/api.py", line 168, in __getattr__
+    value = self.callbacks[name](self)
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/web/chrome.py", line 460, in prepare_request
+    for category, name, text in contributor.get_navigation_items(req):
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/ticket/web_ui.py", line 133, in get_navigation_items
+    if 'TICKET_CREATE' in req.perm:
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 523, in has_permission
+    return self._has_permission(action, resource)
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 536, in _has_permission
+    decision = PermissionSystem(self.env). \
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 424, in check_permission
+    perm)
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 281, in check_permission
+    permissions = PermissionSystem(self.env). \
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 357, in get_user_permissions
+    for perm in self.store.get_user_permissions(username):
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/perm.py", line 175, in get_user_permissions
+    cursor.execute("SELECT username,action FROM permission")
+  File "/usr/lib/python2.4/site-packages/Trac-0.11-py2.4.egg/trac/db/util.py", line 51, in execute
+    return self.cursor.execute(sql)
+  File "/usr/lib64/python2.4/site-packages/sqlite/main.py", line 244, in execute
+    self.rs = self.con.db.execute(SQL)
+OperationalError: database is locked
