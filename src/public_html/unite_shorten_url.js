21dde0de20569f1ce37a749bfd6c72d5fcdc1034
Atsushi Takayama <atsushi@wpa114099.wireless.mcgill.ca>
2009-10-28 11:55:52 -0400

    do not double request

--- src/public_html/unite_shorten_url.js
+++ src/public_html/unite_shorten_url.js
@@ -1,20 +1,23 @@
 (function(){
   var _updateCount = window.updateCount;
+  var requesting = false;
 
   window.updateCount = function(){
     var fst = $('fst');
-    if (/((https?:\/\/[^\s]+?)(;;;|；；；))/.test(fst.value)) {
+    if (!requesting && /((https?:\/\/[^\s]+?)(;;;|；；；))/.test(fst.value)) {
       var command = RegExp.$1;
       var originalUrl = RegExp.$2;
-      if (originalUrl.indexOf('http://j.mp/') === 0) {
+      if (originalUrl.indexOf('http://j.mp/') === 0 && originalUrl.indexOf('http://bit.ly/') === 0) {
         var requestUrl = './resolveurl?url='+encodeURIComponent(originalUrl);
       } else {
         var requestUrl = './shorten?url='+encodeURIComponent(originalUrl);
       }
       var xhr = new XMLHttpRequest;
       xhr.open('GET', requestUrl, true);
+      requesting = true;
       xhr.onload = function(){ 
-        if (xhr.responseText) {
+        requesting = false;
+        if (xhr.responseText && fst.value.indexOf(command) >= 0) {
           fst.value = fst.value.replace(command, xhr.responseText);
         }
         _updateCount();
